1aef071fe8d880224a7f0a84032884b337870aaf
Perawatt <pekung4433@perfenterprise.com>
2018-09-22 15:15:51 +0700

    fix model

--- src/MeetMe/src/app/model.ts
+++ src/MeetMe/src/app/model.ts
@@ -8,13 +8,14 @@
       export class Meeting {
         constructor(
           public _id: string,
-          public topic: string
+          public groupid : string,
+          public topic: string,
+          public details: string
         ) { }
       }
       export class MyGroup {
         constructor(
           public _id: string,
           public name: string,
-          public groupid : string
         ) { }
       }
